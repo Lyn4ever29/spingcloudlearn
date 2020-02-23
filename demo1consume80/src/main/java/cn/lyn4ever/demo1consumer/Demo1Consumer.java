539849e1d31470a7e5b6429ffb00c92a31c5eebf
lyn4ever29 <Lyn4ever29@163.com>
2020-02-24 00:17:36 +0800

    Initial commit

---
+++ demo1consume80/src/main/java/cn/lyn4ever/demo1consumer/Demo1Consumer.java
@@ -0,0 +1,11 @@
+package cn.lyn4ever.demo1consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Consumer {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Consumer.class,args);
+    }
+}
---
+++ demo1provider8001/src/main/java/cn/lyn4ever/Demo1Provider.java
@@ -0,0 +1,11 @@
+package cn.lyn4ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Provider {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Provider.class, args);
+    }
+}
